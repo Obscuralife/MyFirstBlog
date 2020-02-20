dc134e855463da3010b9eba2aeb152f6c9bf1746
Aliaksandr Shakavets <Aliaksandr_Shakavets@epam.com>
2020-02-20 18:19:51 +0300

    Init WebApi prj. Add swashbuckle, DB settings

---
+++ MyBlog.WebApi/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBlog.WebApi
+{
+    public class Settings
+    {
+        public string ConnectionString { get; set; }
+        public string Database { get; set; }
+        public string EntriesCollection { get; set; }
+        public string CommentsCollection { get; set; }
+    }
+}
